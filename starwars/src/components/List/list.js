3b97820e4f2c54614d92f83ff8149b7646042842
Olenka <ovargasl@unsa.edu.pe>
2020-12-08 22:10:21 -0500

    feat: listview container

---
+++ starwars/src/components/List/list.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './list.css';
+
+class List extends React.Component {
+    render(){
+        return (
+            <div className="ListView">
+            </div>
+        )
+    }
+}
+
+export default List
--- starwars/src/index.js
+++ starwars/src/index.js
@@ -5,15 +5,19 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 import Header from './components/Header/header'
+import List from './components/List/list'
 
 const container = document.getElementById('root')
 
 ReactDOM.render(
     <div>
         <Header />
+        <div>
+            <List />
+        </div>
+
     </div>
-    ,
-  container
+    ,  container
 );
 
 
