85af36d0c966a417078c3f261c4eacf703a30d28
Olenka <ovargasl@unsa.edu.pe>
2020-12-10 01:23:25 -0500

    feat: apollo and configurations, getAllPeople, listview

--- starwars/src/components/List/list.js
+++ starwars/src/components/List/list.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { Component }  from 'react';
 import './list.css';
+import {useQuery} from "@apollo/client";
+import {GET_ALL_PEOPLE} from "../../api/query";
 
-class List extends React.Component {
+
+
+function AllPeople() {
+    const { loading, error, data } = useQuery(GET_ALL_PEOPLE);
+    if (loading) return <p className="Loading">Loading...</p>;
+    if (error) return <p className="ErrorData">Failed to Load Data</p>;
+
+    return data.allPeople.people.map((item, index) => (
+        <div key={item.id}>
+            <div className="card">
+                <div className="container">
+                    <p className="TitleLabel">
+                        {item.name}
+                    </p>
+                    <p className="DescriptionLabel">
+                        {item.species? item.species.name: 'Human'} from { item.homeworld? item.homeworld.name: 'Human' }
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    ));
+}
+
+class List extends Component {
+
     render(){
         return (
             <div className="ListView">
+                    <AllPeople />
             </div>
         )
     }
--- starwars/src/index.js
+++ starwars/src/index.js
@@ -4,19 +4,27 @@ import './index.css';
 //import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+import { client } from './api/resource'
+import { ApolloProvider } from "@apollo/client";
+
 import Header from './components/Header/header'
 import List from './components/List/list'
 
 const container = document.getElementById('root')
 
-ReactDOM.render(
-    <div>
-        <Header />
-        <div>
-            <List />
-        </div>
 
-    </div>
+function Main() {
+    return (
+        <ApolloProvider client={client}>
+            <Header />
+            <List />
+        </ApolloProvider>
+    );
+}
+
+ReactDOM.render(
+    <Main />
     ,  container
 );
 
